eb4f58a248192fceceb446deaa39e92498192092
Filip <fakenews.comp@gmail.com>
2021-05-25 14:37:54 +0200

    add main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,22 @@
 package com.company;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Test");
-
+        Main main = new Main();
+        main.start();
+    }
+    public Main(){
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(200,200);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+    private void start(){
+        System.out.println("Start");
     }
 }
