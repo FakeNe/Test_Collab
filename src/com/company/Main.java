2d607c4ad1e978cbe1fb1428ffbbaada3f134c09
Filip <fakenews.comp@gmail.com>
2021-05-25 15:16:08 +0200

    Update structure

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,19 +4,24 @@ import javax.swing.*;
 
 public class Main {
 
+    private JFrame frame;
+
     public static void main(String[] args) {
         System.out.println("Test");
         Main main = new Main();
         main.start();
     }
     public Main(){
-        JFrame frame = new JFrame();
+        createGUI();
+    }
+    private void start(){
+        frame.setVisible(true);
+    }
+
+    private void createGUI() {
+        frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400,400);
         frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-    }
-    private void start(){
-        System.out.println("Start main");
     }
 }
